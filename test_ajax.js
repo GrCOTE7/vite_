3f0c3ccc288be07f6e191f7a61ce2d3aaa4f2933
Lionel COTE <grcote7@gmail.com>
2022-10-07 20:57:41 +0200

    Feat: Base graphisme

---
+++ test_ajax.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  let myDiv = document.getElementsByTagName('div')[0]
+  let myDivInit = myDiv
+  console.log(myDivInit)
+  // myDiv.innerHTML = 'Yes'
+  console.log(myDiv)
+}
